ef4190b5c35e1c8958c1d7ce419e83d6b70fc66c
aaditya saini <aadityasaini@aadityas-MacBook-Air.local>
2026-05-03 15:28:11 +0530

    fix api url

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_URI = import.meta.env.VITE_APP_BASE_URL;
+const API_URI = import.meta.env.VITE_APP_BASE_URL || "http://localhost:5000";
 
 const baseQuery = fetchBaseQuery({ baseUrl: `${API_URI}/api` });
 
